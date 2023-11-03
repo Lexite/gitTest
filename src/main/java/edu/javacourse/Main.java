6ad92be0f3649acc65450a95d583d1c0624902ad
Alexei Rusnac <alexeirusnac1997@gmail.com>
2023-11-03 14:09:18 +0200

    Something has been done 1

--- src/main/java/edu/javacourse/Main.java
+++ src/main/java/edu/javacourse/Main.java
@@ -3,5 +3,6 @@ package edu.javacourse;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("something has been done 1");
     }
 }
